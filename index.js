1f78a12298ee772dccaff8cfcfb6188cfa8e750f
David Schissler <david.schissler@gmail.com>
2014-08-29 12:43:39 +0300

    Removed some commented out debug lines for first commit.

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ var po2json = require('po2json');
 module.exports = function(source) {
 	this.cacheable();
 
-// console.log(source);
-// process.exit(1);
-
 	jsonData = po2json.parse(source, {stringify:true});
-// console.log("Made it");
-// console.log(jsonData);
-// process.exit(1);
 
 	return jsonData;
 }
