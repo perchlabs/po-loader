38174c6fd4cc32845ae2ed1ba748633bb92276db
David Schissler <david.schissler@gmail.com>
2017-06-23 08:19:04 -0700

    Change tabs to spaces.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author David Schissler @dschissler
+    MIT License http://www.opensource.org/licenses/mit-license.php
+    Author David Schissler @dschissler
 */
 var po2json = require('po2json');
 var utils = require('loader-utils');
 
 module.exports = function(source) {
-	this.cacheable();
+    this.cacheable();
 
-        var options = utils.getOptions(this);
+    var options = utils.getOptions(this);
 
-	if (options === null) {
-		options = {};
+    if (options === null) {
+        options = {};
+    }
+
+    // default option
+    if (!('stringify' in options)) {
+        options.stringify = true;
 	}
 
-	// default option
-	if (!('stringify' in options)) {
-		options.stringify = true;
-	}
+    jsonData = po2json.parse(source, options);
 
-	jsonData = po2json.parse(source, options);
-
-	return jsonData;
+    return jsonData;
 }
